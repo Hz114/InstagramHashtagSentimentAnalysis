23176c5af0dbc472b5bddd2314bf632e20843cb9
Hz114 <kauphj17@gmail.com>
2021-04-07 10:25:11 +0900

    Add Crawler python

--- InstagramHashtagSentimentAnalysisApp/tests.py
+++ InstagramHashtagSentimentAnalysisApp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from bs4 import BeautifulSoup
+import selenium.webdriver as webdriver
+import urllib.parse
+from urllib.request import Request, urlopen
+from time import sleep
+import pandas as pd
 
 # Create your tests here.
--- InstagramHashtagSentimentAnalysisApp/views.py
+++ InstagramHashtagSentimentAnalysisApp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
+
+def result(request):
+    return render(request, 'result.html')
+
